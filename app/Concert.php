0c02a82b68f69fc7863d21128d62b4320778ef23
amite <amit.erandole@gmail.com>
2016-12-02 17:41:34 +0530

    customers can purchase concert tickets

--- app/Concert.php
+++ app/Concert.php
@@ -31,4 +31,9 @@ class Concert extends Model
     {
       return $query->whereNotNull('published_at');
     }
+
+    public function orders()
+    {
+      return $this->hasMany(Order::class);
+    }
 }
